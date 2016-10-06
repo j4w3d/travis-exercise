7d8fb34969ca2ba1b2e268837c90599aec6b232b
Jawed Salim <jawed.eternal@gmail.com>
2016-10-06 17:44:28 +0530

    Update send-mail.sh

--- send-mail.sh
+++ send-mail.sh
@@ -1,2 +1,2 @@
 sudo apt-get install postfix mailutils
-echo "JUST A TRAVIS TEST" | mail -s "TRAVIS: a test" jawed.salim@apyl.com
+echo "JUST A TRAVIS TEST" | mail -s "TRAVIS: a test" jawed.eternal@gmail.com
